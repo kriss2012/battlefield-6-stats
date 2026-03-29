c21bb97d31555bf8b9df09d5dd9767f55629bd1e
Krishna Patil <krishnacpatil2006@gmail.com>
2026-03-29 18:29:34 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { z } from 'zod';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import { createServer } from 'http';
