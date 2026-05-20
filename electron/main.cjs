e93cafe05baf9ea4035ec2077297e3392e9d7423
Krishna Patil <krishnacpatil2006@gmail.com>
2026-05-20 16:15:41 +0530

    Create main.cjs

---
+++ electron/main.cjs
@@ -0,0 +1,67 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    autoHideMenuBar: true
+  });
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  // Handle escape to exit fullscreen
+  mainWindow.on('enter-full-screen', () => {
+    globalShortcut.register('Escape', () => {
+      mainWindow.setFullScreen(false);
+    });
+  });
+
+  mainWindow.on('leave-full-screen', () => {
+    globalShortcut.unregister('Escape');
+  });
+
+  // Fallback global shortcut just in case
+  globalShortcut.register('Escape', () => {
+    if (mainWindow.isFullScreen()) {
+      mainWindow.setFullScreen(false);
+    }
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  globalShortcut.unregisterAll();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
