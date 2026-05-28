ab20b171ee2784b4e3d5600b4e77aded173865c1
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-28 20:11:37 +0530

    Update SocketContext.tsx

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,6 +1,7 @@
 /**
  * Working: This file provides core functionality and UI rendering for its specific module.
  * Use: Imported and utilized across the application as part of the game's system logic.
+ * #by Kiri Team
  */
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
