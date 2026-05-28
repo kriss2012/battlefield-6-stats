f1f560570efb3e66e875701ae7dbae85c5db91c3
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-28 20:47:33 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 /**
  * Working: This file provides core functionality and UI rendering for its specific module.
  * Use: Imported and utilized across the application as part of the game's system logic.
+ * File: Footer.tsx
+ * Date: 2026-05-28
  * #by Kiri Team
  */
 import { Link } from 'react-router-dom';
