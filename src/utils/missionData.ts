599771d943254ce10bc0e6efd43145e17dc781cc
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:51:27 +0530

    Update missionData.ts

--- src/utils/missionData.ts
+++ src/utils/missionData.ts
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: missionData.ts
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 export interface Mission {
   id: string;
