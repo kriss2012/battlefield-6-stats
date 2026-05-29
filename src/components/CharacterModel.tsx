46c056d5550305e2f26926300035be6913fb77f4
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:48:32 +0530

    Update CharacterModel.tsx

--- src/components/CharacterModel.tsx
+++ src/components/CharacterModel.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: CharacterModel.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import React, { useRef, Suspense } from 'react';
 import { useFrame } from '@react-three/fiber';
