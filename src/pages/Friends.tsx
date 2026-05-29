dba50b76162f123f3318ab7798803031a0b7f2fd
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:50:12 +0530

    Update Friends.tsx

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: Friends.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { useState, useEffect, useCallback } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
