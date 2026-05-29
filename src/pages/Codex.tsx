f7ea2eeaf88cdfa42a6770a25d633cce44e3eea3
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:50:08 +0530

    Update Codex.tsx

--- src/pages/Codex.tsx
+++ src/pages/Codex.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: Codex.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
