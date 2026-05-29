23978bacc4f1fc208c2409b8f8c38a9c9bf0a5ae
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:51:23 +0530

    Update PlayerAnalytics.tsx

--- src/pages/PlayerAnalytics.tsx
+++ src/pages/PlayerAnalytics.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: PlayerAnalytics.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { useState, useEffect } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
