294ac805754336bd448e723cb5ee6e9acafc8000
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:49:45 +0530

    Update WinRateTrendChart.tsx

--- src/components/WinRateTrendChart.tsx
+++ src/components/WinRateTrendChart.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: WinRateTrendChart.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { useEffect, useState } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
