ae21330ac76ce9cbeca40ed5f3e14914e95c5e84
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:50:33 +0530

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: Profile.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
