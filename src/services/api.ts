919d7cd3ae757e7bb4216bc306969f4762242d98
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:51:04 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: api.ts
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 // BF6 API Service Configuration
 
