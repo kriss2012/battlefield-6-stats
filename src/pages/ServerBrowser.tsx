5d11988f738845a05c1b25faaa61438f3d29fe70
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:50:42 +0530

    Update ServerBrowser.tsx

--- src/pages/ServerBrowser.tsx
+++ src/pages/ServerBrowser.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: ServerBrowser.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
