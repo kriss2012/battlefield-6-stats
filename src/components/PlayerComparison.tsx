5ffc68960eed0ee0d2c1cb1538b7b71e3b49b7d2
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:49:14 +0530

    Update PlayerComparison.tsx

--- src/components/PlayerComparison.tsx
+++ src/components/PlayerComparison.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: PlayerComparison.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { useState } from 'react';
 import { analyticsApi } from '../services/api';
