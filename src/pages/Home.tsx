77004fb0d03ff1bda05b9588c13ab94182abf3a3
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:50:19 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: Home.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
