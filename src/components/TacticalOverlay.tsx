2dadf55d4558648fd380912dafed30ba5add783f
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:49:34 +0530

    Update TacticalOverlay.tsx

--- src/components/TacticalOverlay.tsx
+++ src/components/TacticalOverlay.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: TacticalOverlay.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import React, { useEffect, useState, useCallback } from 'react';
 import { motion, useScroll, useTransform, useSpring, useMotionValue } from 'framer-motion';
