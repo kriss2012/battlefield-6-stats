419ad294fee4a1a40a15115b2bee154efc1c03af
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:48:59 +0530

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: Logo.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import React from 'react';
 
