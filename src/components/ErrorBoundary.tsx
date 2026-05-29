efab5067c3953ff87faab05b0ba455f071cd83e0
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:48:35 +0530

    Update ErrorBoundary.tsx

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: ErrorBoundary.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { Component, type ErrorInfo, type ReactNode } from 'react';
 
