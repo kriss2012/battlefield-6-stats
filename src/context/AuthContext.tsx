d6677a061c82a4b9e69edba5287f4d10b31d4548
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-29 19:49:51 +0530

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,7 +3,7 @@
  * Use: Imported and utilized across the application as part of the game's system logic.
  * File: AuthContext.tsx
  * Date: 2026-05-28
- * #by Kiri Team
+ * #Made WIth Love TO The Kiri Family
  */
 import { createContext, useContext, useState, useEffect, type ReactNode } from 'react';
 import { authApi } from '../services/api';
